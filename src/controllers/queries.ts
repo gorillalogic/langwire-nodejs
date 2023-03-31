f8e1dac8b7253cbee04d7c41ae32fdbfa3d6258d
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-03-31 15:14:38 -0600

    Add index placeholders

--- src/controllers/queries.ts
+++ src/controllers/queries.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
+import Query from '../models/query';
+import mapper from '../utils/dynamodb';
 
 const Controller = {
-    index: (_req: Request, res: Response) => {
+    index: async (_req: Request, res: Response) => {
+        const iterator = mapper.scan(Query);
+        let queries = [];
+        for await (const item of iterator) {
+            queries.push(item);
+        }
+
         res.status(200).render('queries/index', {
             pageTitle: 'Queries',
-        })
+            queries: queries,
+        }); 
     },
 }
 
