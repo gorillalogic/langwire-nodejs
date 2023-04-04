c1f360072ef954cdf7b47989fd84aee1f17849f6
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-04-04 12:56:05 -0600

    Limited creation of queries

--- app/controllers/queries.ts
+++ app/controllers/queries.ts
@@ -2,22 +2,30 @@ import { Request, Response } from 'express';
 import Query from '../models/query';
 import mapper from '../utils/dynamodb';
 
-const Controller = {
-    index: async (_req: Request, res: Response) => {
-        const iterator = mapper.scan(Query);
-        let queries = [];
-        for await (const item of iterator) {
-            queries.push(item);
-        }
+export const index = async (_req: Request, res: Response) => {
+    const iterator = mapper.scan(Query);
+    let queries = [];
+    for await (const item of iterator) {
+        queries.push(item);
+    }
 
-        res.status(200).render('../views/queries/index', {
-            pageTitle: 'Queries',
-            queries: queries,
-            titleFields: [
-                "id", "form", "lemma", "cpostag", "postag", "feats", "head", "deprel", "phead", "pdeprel"
-            ]
-        }); 
-    },
+    res.status(200).render('../views/queries/index', {
+        pageTitle: 'Queries',
+        queries: queries,
+        titleFields: [
+            "id", "form", "lemma", "cpostag", "postag", "feats", "head", "deprel", "phead", "pdeprel"
+        ]
+    });
+};
+
+export const create = async (req: Request, res: Response) => {
+    const toSave = Object.assign(new Query, {
+        contents: req.body.contents,
+        output_conll: "[]",
+        user_id: 1,
+    })
+    await mapper.put(toSave)
+    res.redirect('/queries')
 }
 
-export default Controller;
+export default { index, create };
--- app/routes/queries.ts
+++ app/routes/queries.ts
@@ -4,5 +4,6 @@ import QueriesController from "../controllers/queries";
 const router = Router();
 
 router.get("/", QueriesController.index);
+router.post("/", QueriesController.create);
 
 export default router;
